1e9663cd610c83a3073a433c86086db3c2cb9ca0
ppphongqi <2503389490@qq.com>
2019-08-23 15:56:24 +0800

    router vuex

--- src/main.js
+++ src/main.js
@@ -1,8 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
+import router from './router'
+import Vuex from './store';
 
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  el: '#app',
+  router,
+  Vuex,
+  template:'<App/>',
+  components: {App},
+})
