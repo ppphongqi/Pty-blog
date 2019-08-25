392f62f304e5787b3fa895751109b7b59f3358f0
ppphongqi <2503389490@qq.com>
2019-08-25 22:05:15 +0800

    little change

--- src/main.js
+++ src/main.js
@@ -2,8 +2,10 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import Vuex from './store';
+import ElementUI from 'element-ui';
 
 Vue.config.productionTip = false
+Vue.use(ElementUI);
 
 new Vue({
   el: '#app',
